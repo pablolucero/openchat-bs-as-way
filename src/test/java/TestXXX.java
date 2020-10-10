eae9e17fd25839869d6f04629136ed6b09e38f31
Pablo Lucero <plucero87@gmail.com>
2020-10-10 03:49:05 -0300

    isNamed works as expected

--- src/test/java/TestXXX.java
+++ src/test/java/TestXXX.java
@@ -1,8 +1,14 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TestXXX {
+
+    public static final String PEPE_SANCHEZ_NAME = "Pepe Sanchez";
+    public static final String PEPE_SANCHEZ_PASSWORD = "password";
 
     @Test
     void publisherCanNotHaveBlankName() {
@@ -16,8 +22,19 @@ class TestXXX {
 
     @Test
     void canCreatePublisherWithNoBlankName() {
-        Publisher createdPublisher = Publisher.named("Pepe Sanchez", "password", "about");
+        Publisher createdPublisher = createPepeSanchez();
 
-        assertTrue(createdPublisher.isNamed("Pepe Sanchez"));
+        assertTrue(createdPublisher.isNamed(PEPE_SANCHEZ_NAME));
+    }
+
+    @Test
+    void isNamedReturnsFalseWhenAskedWithOtherName() {
+        Publisher createdPublisher = createPepeSanchez();
+
+        assertFalse(createdPublisher.isNamed("Juan"));
+    }
+
+    private Publisher createPepeSanchez() {
+        return Publisher.named(PEPE_SANCHEZ_NAME, PEPE_SANCHEZ_PASSWORD, "about");
     }
 }
